f9888f79356664af0936cc8042ff76ee66cae67f
snir2g1 <snir2g1@localhost>
2022-01-10 12:47:25 +0100

    Premier commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: snir2g1
+ *
+ * Created on 10 janvier 2022, 12:42
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Bonjour" << endl;
+    cout << "monde ! " << endl;
+    return 0;
+}
+
